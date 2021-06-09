5cca1e84887c05fd08fb5b72a427fc711fc2dbd2
sreehanrm2009 <83212024+sreehanrm2009@users.noreply.github.com>
2021-06-09 16:14:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+console.log("Welcome to console screen!");
+var first_name = "Harry";
+console.log(first_name);
+
+var last_name = " Potter";
+console.log(last_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var length_of_full_name = full_name.length;
+console.log(length_of_full_name);
+
+var replaced_name = full_name.replace("Harry", "Tyler");
+console.log(replaced_name);
+
+var a = 4;
+var b = 8;
+console.log(a);
+console.log(b);
+
+var add = a + b;
+console.log(add);
+
+var subtract = b - a;
+console.log(subtract);
+
+var multiply = a * b;
+console.log(multiply);
+
+var divide = b / a;
+console.log(divide);
+
+var min = Math.min(a, b);
+console.log(min);
+
+var max = Math.max(a, b);
+console.log(max);
+
+var square_root = Math.sqrt(a);
+console.log(square_root);
+
+var power = Math.pow(b, a);
+console.log(power);
